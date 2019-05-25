49a0638f04f57244f8ce34805ac8ea096b8bae8b
a3y3 <sohamssd@gmail.com>
2019-05-25 19:37:44 -0400

    Add answer 2

--- Log Analysis/analysis.py
+++ Log Analysis/analysis.py
@@ -8,18 +8,7 @@ from articles left join log \
 on log.path like '%'||articles.slug||'%';"
 
 
-def solve_question1():
-    """
-    Groups the view on the basis of title and calculates the count.
-    :return: the executed result (table of title and count)
-    """
-    print("What are the most popular three articles of all time?")
-    query = "select title, count(path) as count \
-                from article_counts \
-                group by title \
-                order by count desc \
-                limit 3;"
-
+def execute(query):
     t_start = time.time()
     c.execute(query)
     values = c.fetchall()
@@ -29,12 +18,42 @@ def solve_question1():
     return values, t_first
 
 
+def print_answer(answer, time):
+    for a in answer:
+        print("\t\"{}\" - {}".format(a[0], a[1]))
+    print("Time taken:", time, "\n")
+
+
+def solve_question1():
+    """
+    Groups the view on the basis of title and calculates the count.
+    :return: the executed result (table of title and count)
+    """
+    print("What are the most popular three articles of all time?")
+    query = """select title, count(path) as count 
+                from article_counts 
+                group by title 
+                order by count desc 
+                limit 3;"""
+
+    return execute(query)
+
+
 def solve_question2():
     """
-
-    :return:
+    Joins the view (article_counts) and authors, groups on the basis of names, and counts the
+    number of titles by each author.
+    :return: result of executed query (name and count)
     """
-    return None
+    print("Who are the most popular article authors of all time?")
+    query = """
+            select name, count(*)
+            from authors join article_counts 
+            on authors.id = article_counts.author
+            group by name
+            order by count(*) DESC;
+            """
+    return execute(query)
 
 
 def solve_question3():
@@ -46,7 +65,7 @@ if __name__ == '__main__':
     c = db.cursor()
 
     answer1, time1 = solve_question1()
+    print_answer(answer1, time1)
 
-    for answer in answer1:
-        print("\t{} - {}".format(answer[0], answer[1]))
-    print("\nDone. Time taken for first query:", time1)
+    answer2, time2 = solve_question2()
+    print_answer(answer2, time2)
