726b8637e4f0dd5be9bd3ef05c5846af8f541632
a3y3 <sohamssd@gmail.com>
2019-05-26 15:42:10 -0400

    Fix trailing whitespaces

--- analysis.py
+++ analysis.py
@@ -5,9 +5,11 @@ import psycopg2
 
 def execute(query):
     """
-    Executes a given query. Note that the function will re-use the cursor defined in main.
+    Executes a given query. Note that the function will re-use the cursor
+    defined in main.
     :param query: a query that needs to be executed by the cursor.
-    :return: the result of the query using cursor.fetchall(), and the time taken to execute the query.
+    :return: the result of the query using cursor.fetchall(), and the time
+    taken to execute the query.
     """
     db = psycopg2.connect(database="news")
     c = db.cursor()
@@ -29,10 +31,10 @@ def get_top_articles():
     """
     print("What are the most popular three articles of all time?")
     query = """
-            select title, count(path) as count 
-            from article_counts 
-            group by title 
-            order by count desc 
+            select title, count(path) as count
+            from article_counts
+            group by title
+            order by count desc
             limit 3;
             """
 
@@ -41,14 +43,15 @@ def get_top_articles():
 
 def get_top_authors():
     """
-    Joins the view (article_counts) and authors, groups on the basis of names, and counts the
+    Joins the view (article_counts) and authors, groups on the basis of names,
+    and counts the
     number of titles by each author.
     :return: result of executed query (name and count)
     """
     print("Who are the most popular article authors of all time?")
     query = """
             select name, count(*)
-            from authors join article_counts 
+            from authors join article_counts
             on authors.id = article_counts.author
             group by name
             order by count(*) DESC;
@@ -58,14 +61,16 @@ def get_top_authors():
 
 def get_days_with_1_percent_errors():
     """
-    Joins two predefined views and calculates percentage errors as total_errors/total_requests
+    Joins two predefined views and calculates percentage errors as
+    total_errors/total_requests
     (where total_errors and total_requests are columns in the views)
     :return: result in the form of day and percent errors.
     """
     print("On which days did more than 1% of requests lead to errors?")
     query = """
-            select total_req.day, (total_errors::decimal/total_requests*100) as percent
-            from total_req join total_req_err 
+            select total_req.day, (total_errors::decimal/total_requests*100)
+            as percent
+            from total_req join total_req_err
             on total_req.day = total_req_err.day
             where total_errors::decimal/total_requests*100 >= 2;
             """
